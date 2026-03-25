10395571834648439b8264ca8b9dca4ddd15187b
tekesbenson <tekesbenson@gmail.com>
2026-03-25 10:39:02 +0300

    add updated files

---
+++ lesson1a.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // A variable is a container that stores data or values.
+    // In Kotlin we have two types of initializers i.e. var and val
+    // val => immutable (unchangeable) but var => changeable
+
+    val name = "Benson Seyanga"   // String => series of characters
+    val number = 5             // Integer => whole number
+    val number2 = 40.6         // Double => decimal number
+    var letter = 'k'           // Char => single character
+    val isRegistered = true    // Boolean
+
+    println(name)
+    println(number)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+}
---
+++ lesson1b.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    // Below we declare variables and then we state their data types
+    val number1 :Int = 5
+    val number2 :Double = 5.0   
+    val letter :Char = 'B'     
+    val paidLoan :Boolean = false
+    val name :String ="James"
+
+    // Print out the output
+    println(number1)
+    println(number2)
+    println(letter)
+    println(paidLoan)
+    println(name)
+}
